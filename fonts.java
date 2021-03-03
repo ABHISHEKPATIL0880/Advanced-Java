9bfd751968db89eb82dd30161f6ffef1652aab28
ABHISHEKPATIL0880 <64071321+ABHISHEKPATIL0880@users.noreply.github.com>
2021-03-03 12:33:23 +0530

    Update fonts.java

--- fonts.java
+++ fonts.java
@@ -1,17 +1,17 @@
 package abhishek.learn;
 
 import java.awt.GraphicsEnvironment;
-class fonts
+public class fonts
 {
-    //Driver Function
+   
     public static void main(String[] args)
     {
-        //Get the local graphics environment
+        
         GraphicsEnvironment local_env;
         local_env= GraphicsEnvironment.getLocalGraphicsEnvironment();
-        //Get available font names
+        
         String allfonts[] = local_env.getAvailableFontFamilyNames();
-        //Display the list of fonts
+        
         for(int i=0;i<allfonts.length;i++)
             System.out.println(allfonts[i]);
     }
