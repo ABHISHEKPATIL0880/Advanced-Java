51d84b60b3cde45871550ce201bafc8443d8679f
ABHISHEKPATIL0880 <64071321+ABHISHEKPATIL0880@users.noreply.github.com>
2021-03-03 12:23:11 +0530

    Update smile.java

--- smile.java
+++ smile.java
@@ -1,4 +1,5 @@
-package abhishek.learn;
+
+      package abhishek.learn;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -20,9 +21,8 @@ class smile extends Frame
 
     public void paint(Graphics g)
     {
-        g.setColor(Color.red);
-        g.fillRect(50,50,200,200);
-        g.drawRect(50,50,200,200);
+
+
         g.setColor(Color.yellow);
         g.fillOval(75,75,150,150);
         g.drawOval(75,75,150,150);
@@ -33,20 +33,16 @@ class smile extends Frame
         //eye
         g.fillOval(180,100,15,15);
         g.drawOval(180,100,15,15);
-        //eye
-        g.drawLine(150,135,150,155);
-        //nose
         g.setColor(Color.pink);
         g.fillArc(110,140,75,50,0,-175);
         g.drawArc(110,140,75,50,0,-175);
-        //smile
     }
 
     public static void main(String arg[])
     {
         smile S = new smile();
         S.setSize(400,400);
-        S.setTitle("Drawing");
+        S.setTitle("SMILEY");
         S.setVisible(true);
     }
 
